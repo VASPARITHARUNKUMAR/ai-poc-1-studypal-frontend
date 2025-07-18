4533963d54d55d645f6cd1d9a81422e8576aca2d
Tharun Kumar Vaspari <vasparitharunkumar2004@gmail.com>
2025-07-18 12:09:29 -0400

    frontend code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Backend URL
-BACKEND = "http://localhost:9000"
+BACKEND = "http://localhost:8000"
 
 # Page Configuration
 st.set_page_config(page_title="StudyPal - Smart Assistant", page_icon="📚", layout="wide")
